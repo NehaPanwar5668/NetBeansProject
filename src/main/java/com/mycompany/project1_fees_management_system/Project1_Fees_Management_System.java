e679bbf5a4d4183ab38c3f00892f2ffca9ebeb54
NehaPanwar5668 <nehapanwar5668@gmail.com>
2026-02-11 09:46:54 +0530

    java project implementation.

---
+++ src/main/java/com/mycompany/project1_fees_management_system/Project1_Fees_Management_System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project1_fees_management_system;
+
+/**
+ *
+ * @author nehap
+ */
+public class Project1_Fees_Management_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
